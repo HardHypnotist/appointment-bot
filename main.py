bec19480318e17f52b817531dc0aac43ed2b7add
HardHypnotist <a.rattan2004@gmail.com>
2026-06-10 10:38:25 +0530

    Add WhatsApp auto reply

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 from fastapi import FastAPI, Request
+import requests
+import os
 
 app = FastAPI()
 
 VERIFY_TOKEN = "anmol_bot_2026"
 
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+
+
+def send_whatsapp_message(to_number, message):
+    url = f"https://graph.facebook.com/v23.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": to_number,
+        "type": "text",
+        "text": {
+            "body": message
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    print(response.status_code)
+    print(response.text)
+
+
 @app.get("/")
 def home():
     return {"status": "running"}
+
 
 @app.get("/webhook")
 async def verify(request: Request):
@@ -19,11 +48,26 @@ async def verify(request: Request):
 
     return {"error": "verification failed"}
 
+
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
 
-    print("Received message:")
-    print(data)
+    try:
+        entry = data["entry"][0]
+        changes = entry["changes"][0]
+        value = changes["value"]
+
+        if "messages" in value:
+            message = value["messages"][0]
+            sender = message["from"]
+
+            send_whatsapp_message(
+                sender,
+                "Hello!\n\nBook your appointment here:\nhttps://your-booking-link.com"
+            )
+
+    except Exception as e:
+        print("Error:", e)
 
     return {"status": "received"}
